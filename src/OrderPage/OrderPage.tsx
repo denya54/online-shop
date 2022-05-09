767f642eb242d3e4e72f18d267149903fe7d4fcd
denya54 <denis-khvesenya@yandex.com>
2022-05-09 16:16:14 +0300

    done fields for order active with formik

--- src/OrderPage/OrderPage.tsx
+++ src/OrderPage/OrderPage.tsx
@@ -1,6 +1,15 @@
 import {Button, FormControl, FormGroup, Grid, TextField} from "@mui/material";
 import React from "react";
 import {TitleForPage} from "../Cart/Cart";
+import {useFormik} from "formik";
+
+type FormikErrorType = {
+    name?: string
+    surname?: string
+    address?: string
+    phone?: string
+}
+
 
 
 type OrderPagePropsType = {
@@ -8,53 +17,81 @@ type OrderPagePropsType = {
 }
 
 export const OrderPage = (props: OrderPagePropsType) => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            surname: '',
+            address: '',
+            phone: ''
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {};
+            if (!values.name) {
+                errors.name = 'Required';
+            }
+            if (!values.phone) {
+                errors.phone = 'Required';
+            }
+            return errors;
+        },
+
+        onSubmit: values => {
+            alert(JSON.stringify(values));
+            formik.resetForm()
+        },
+    })
+
+
+
     return (
         <div>
             <TitleForPage text={'Please, write on the forms'}/>
             <Grid container justifyContent={'center'}>
                 <Grid item justifyContent={'center'}>
-                    // формик сюда
+                   <form onSubmit={formik.handleSubmit}>
                     <FormControl>
                         <FormGroup>
                             <TextField
                                 id="outlined-name"
                                 label="Name"
-                                value={''}
                                 margin="normal"
-                                //onChange={handleChange}
+                                {...formik.getFieldProps('name')}
+
+                                onBlur={formik.handleBlur}
                             />
+                            {formik.touched.name && formik.errors.name && <div style={{color: 'red'}}>{formik.errors.name}</div>}
                             <TextField
                                 id="outlined-name"
                                 label="Surname"
-                                value={''}
                                 margin="normal"
-                                //onChange={handleChange}
+                                {...formik.getFieldProps('surname')}
                             />
                             <TextField
                                 id="outlined-name"
                                 label="Address"
-                                value={''}
                                 margin="normal"
-                                //onChange={handleChange}
+                                {...formik.getFieldProps('address')}
                             />
                             <TextField
                                 id="outlined-name"
                                 label="Phone"
-                                value={''}
                                 margin="normal"
-                                //onChange={handleChange}
+                                {...formik.getFieldProps('phone')}
+                                onBlur={formik.handleBlur}
                             />
+                            {formik.touched.phone && formik.errors.phone && <div style={{color: 'red'}}>{formik.errors.phone}</div>}
                             To pay: {props.totalCost}$
                             <Button variant={'contained'}
-                                    onClick={() => alert('thank you for your order')}
                                     color={'success'}
+                                    type={'submit'}
                             >
                                 ORDER
                             </Button>
 
                         </FormGroup>
                     </FormControl>
-                    //формик закрыть
+                   </form>
                 </Grid>
             </Grid>
         </div>
